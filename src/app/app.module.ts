64355d76181a752cfe83be5ee96c70a59b04aca4
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2021-03-20 20:33:06 +0300

    feat: add pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+import { DictionaryComponent } from './features/dictionary/dictionary.component';
+import { GamesComponent } from './features/games/games.component';
+import { StatisticsComponent } from './features/statistics/statistics.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
+import { LoginComponent } from './features/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DictionaryComponent,
+    GamesComponent,
+    StatisticsComponent,
+    NotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
