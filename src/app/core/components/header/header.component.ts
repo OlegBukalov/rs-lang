edce48bb7966d501224bad12c3510d899288deba
Yury Kuzmich <yurakuzmich@gmail.com>
2021-03-21 16:03:33 +0300

    fix: auto fix lint --fix

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-
 export class HeaderComponent {
   constructor() {}
 }
