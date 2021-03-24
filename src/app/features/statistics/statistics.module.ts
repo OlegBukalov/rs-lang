d9d70463871a5c7fab4df649d75a3827cae3a943
Yury Kuzmich <yurakuzmich@gmail.com>
2021-03-24 12:46:48 +0300

    fix: add lasy loading to statistis

--- src/app/features/statistics/statistics.module.ts
+++ src/app/features/statistics/statistics.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { StatisticsComponent } from './statistics.component';
+import { statisticsRoutes } from './statistics-routes'
+
 
 @NgModule({
   declarations: [StatisticsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(statisticsRoutes)],
 })
 export class StatisticsModule {}
