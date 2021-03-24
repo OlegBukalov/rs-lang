f0433dfc894ff5b5925a67849ce05589ff890811
Maryna Migacheva <m.migacheva@gmail.com>
2021-03-24 22:03:04 +0300

    feat: add component home-description

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { HomeDescriptionComponent } from './home-description/home-description.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeDescriptionComponent],
   imports: [CommonModule],
 })
 export class HomeModule {}
