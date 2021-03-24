d538620b7dc7d70d49f1c9ea259a7343df6cb8c1
Maryna Migacheva <m.migacheva@gmail.com>
2021-03-24 22:24:17 +0300

    feat: add component home-advantages

---
+++ src/app/features/home/home-advantages/home-advantages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-advantages',
+  templateUrl: './home-advantages.component.html',
+  styleUrls: ['./home-advantages.component.scss']
+})
+export class HomeAdvantagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeDescriptionComponent } from './home-description/home-description.component';
+import { HomeAdvantagesComponent } from './home-advantages/home-advantages.component';
 
 @NgModule({
-  declarations: [HomeComponent, HomeDescriptionComponent],
+  declarations: [
+    HomeComponent,
+    HomeDescriptionComponent,
+    HomeAdvantagesComponent
+  ],
   imports: [CommonModule],
 })
 export class HomeModule {}
