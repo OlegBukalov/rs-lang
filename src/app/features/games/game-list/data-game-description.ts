8cb9d3b5acf851c77e24409ecbfd92df72174555
Maryna Migacheva <m.migacheva@gmail.com>
2021-03-27 10:57:20 +0300

    fix: use lint

--- src/app/features/games/game-list/data-game-description.ts
+++ src/app/features/games/game-list/data-game-description.ts
@@ -5,20 +5,24 @@ export const GAMES_DESCRIPTION: IGameItem[] = [
     id: 0,
     title: 'Саванна',
     imgUrl: '../assets/img/game-description/game_savanna.jpg',
+    routerLink: 'card-game',
   },
   {
     id: 1,
     title: 'Аудиовызов',
     imgUrl: '../assets/img/game-description/game_audio.jpg',
+    routerLink: 'card-game',
   },
   {
     id: 2,
     title: 'Спринт',
     imgUrl: '../assets/img/game-description/game_sprint.jpg',
+    routerLink: 'card-game',
   },
   {
     id: 3,
     title: 'Своя игра',
     imgUrl: '../assets/img/game-description/game_me.jpg',
+    routerLink: 'card-game',
   },
 ];
