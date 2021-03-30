7a67708c9b01aa9a0d2423b21081c9cbf40b062b
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2021-03-31 00:05:57 +0300

    feat: add TostrService

---
+++ src/app/core/services/toaster.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { IndividualConfig, ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ToasterService {
+  constructor(private toastr: ToastrService) {}
+
+  showSuccess(message: string, title: string): void {
+    this.toastr.success(message, title, {
+      positionClass: 'toast-top-center',
+    });
+  }
+
+  showError(message: string, title: string): void {
+    this.toastr.error(message, title, {
+      positionClass: 'toast-top-center',
+    });
+  }
+
+  showShow(message: string, title: string): void {
+    this.toastr.show(message, title, {
+      positionClass: 'toast-top-center',
+    });
+  }
+
+  showWarning(message: string, title: string): void {
+    this.toastr.warning(message, title, {
+      positionClass: 'toast-top-center',
+    });
+  }
+
+  showInfo(message: string, title: string): void {
+    this.toastr.info(message, title, {
+      positionClass: 'toast-top-center',
+    });
+  }
+
+  showCustomAlert(
+    message: string,
+    title: string,
+    customProperties: Partial<IndividualConfig>,
+  ): void {
+    this.toastr.show(message, title, customProperties);
+  }
+}
