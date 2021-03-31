77dc00011979286105fa1de5b247e07aaa3e3217
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2021-03-31 21:53:22 +0300

    feat: add not found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,12 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './core/components/footer/footer.component';
 import { HeaderComponent } from './core/components/header/header.component';
 
-import { NotFoundComponent } from './features/not-found/not-found.component';
 import { StatisticsModule } from './features/statistics/statistics.module';
 import { HomeModule } from './features/home/home.module';
 import { AuthModule } from './features/auth/auth.module';
 
 @NgModule({
-  declarations: [AppComponent, NotFoundComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     ToastrModule.forRoot(),
