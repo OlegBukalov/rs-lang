c94e3a1cf78fde674299917998d1c206beb166db
Maryna Migacheva <m.migacheva@gmail.com>
2021-03-31 13:09:57 +0300

    fix: back update

--- src/app/features/games/games-routes.ts
+++ src/app/features/games/games-routes.ts
@@ -6,5 +6,6 @@ import { ExitCardGameGuard } from './guards/exit-card-game.guard';
 
 export const gamesRoutes: Routes = [
   { path: '', component: GameListComponent },
+  { path: 'card-game', component: CardGameListComponent, canDeactivate: [ExitCardGameGuard] },
   { path: '**', component: NotFoundComponent },
 ];
