95758bee154bfb254f0f0a078a405430b481c6e3
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2021-04-04 22:55:37 +0300

    feat: add timer

--- src/app/features/games/sprint-game/start-loading/start-loading.component.ts
+++ src/app/features/games/sprint-game/start-loading/start-loading.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { interval } from 'rxjs';
+
+const MaxSeconds = 3;
 
 @Component({
   selector: 'app-start-loading',
   templateUrl: './start-loading.component.html',
   styleUrls: ['./start-loading.component.scss'],
 })
-export class StartLoadingComponent {}
+export class StartLoadingComponent implements OnInit {
+  @Output() startLoadingEnd = new EventEmitter();
+
+  curSecond = MaxSeconds;
+
+  ngOnInit(): void {
+    const timer$ = interval(1000);
+    const sub = timer$.subscribe((sec) => {
+      if (MaxSeconds - 1 === sec) {
+        sub.unsubscribe();
+        this.startLoadingEnd.emit();
+      }
+      this.curSecond -= 1;
+    });
+  }
+}
