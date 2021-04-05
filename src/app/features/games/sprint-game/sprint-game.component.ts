ba04c60c83ebc84f0c4a764667fa74b14d671d83
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2021-04-05 16:08:54 +0200

    feat: add services

--- src/app/features/games/sprint-game/sprint-game.component.ts
+++ src/app/features/games/sprint-game/sprint-game.component.ts
@@ -1,5 +1,9 @@
-import { GameStatuses } from './enums/game-statuses.enum';
 import { Component } from '@angular/core';
+import { IWord } from 'src/app/core/interfaces/iword';
+import { ISprintWord } from './interfaces/sprint-word';
+import { GameStatuses } from './enums/game-statuses.enum';
+import { WordsApiService } from 'src/app/core/services/wordsApi.service';
+import { ToasterService } from 'src/app/core/services/toaster.service';
 
 @Component({
   selector: 'app-sprint-game',
@@ -8,19 +12,59 @@ import { Component } from '@angular/core';
 })
 export class SprintGameComponent {
   score = 0;
-  level = 1;
-  gameStatus = GameStatuses.Start;
+  gameStatus = GameStatuses.Play;
+  words: IWord[];
+  gameWords: ISprintWord[] = [];
+  currentWord: ISprintWord;
 
-  private setLoadingStatus(lvl: number): void {
+  constructor(
+    private wordsApiService: WordsApiService,
+    private toastrService: ToasterService
+  ) {
+    this.currentWord = {
+      id: '',
+      word: '',
+      translate: '',
+      randomTranslate: '',
+      correctTranslate: false,
+      audio: '',
+    };
+  }
+
+  setLoadingStatus(): void {
     this.gameStatus = GameStatuses.Loading;
-    this.level = lvl;
   }
 
-  private setPlayStatus(): void {
+  setPlayStatus(): void {
     this.gameStatus = GameStatuses.Play;
+    this.wordsApiService.getWordList().subscribe(
+      (words: IWord[]) => {
+        if (words) {
+          this.words = words.sort(() => Math.random() - 0.5);
+          this.words.forEach((word, i) => {
+            const isRandom: boolean = Math.random() < 0.5;
+            let randomTranslate = isRandom ? this.words[Math.round(Math.random() * (this.words.length-1))].wordTranslate : word.wordTranslate;
+            this.gameWords.push({
+              id: word.id,
+              word: word.word,
+              translate: word.wordTranslate,
+              randomTranslate: randomTranslate,
+              correctTranslate: randomTranslate === word.wordTranslate,
+              audio: word.audio,
+            });
+          });
+          this.currentWord = this.gameWords[0];
+        }
+    },
+    (err) => this.toastrService.showError(err, 'Не удалось получить слова для изучения, попробуйте позже.')
+    )
   }
 
-  private setEndStatus(): void {
+  setEndStatus(): void {
     this.gameStatus = GameStatuses.End;
+  }
+
+  checkAnswer(): void {
+
   }
 }
