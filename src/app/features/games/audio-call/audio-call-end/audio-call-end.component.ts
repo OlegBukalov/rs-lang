c7074bb8b7f7f11e017d33b4d16e191b9331b970
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2021-04-08 00:50:43 +0300

    Revert " WIP: Feat audio call create some functionality"

--- src/app/features/games/audio-call/audio-call-end/audio-call-end.component.ts
+++ src/app/features/games/audio-call/audio-call-end/audio-call-end.component.ts
@@ -1,16 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-audio-call-end',
   templateUrl: './audio-call-end.component.html',
   styleUrls: ['./audio-call-end.component.scss'],
 })
-export class AudioCallEndComponent {
-  @Input() totalScores: number;
-
-  @Output() beginNewGame = new EventEmitter();
-
-  startNewGame() {
-    this.beginNewGame.emit();
-  }
-}
+export class AudioCallEndComponent {}
--- src/app/features/games/audio-call/audio-call.component.ts
+++ src/app/features/games/audio-call/audio-call.component.ts
@@ -1,9 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
-
-import { Subscription } from 'rxjs';
-import { AudioCallService } from './audio-call.service';
-import { IWord } from '../../../core/interfaces/iword';
-import { IWordsInCallGame, IWordTask } from './interfaces';
+import { Component } from '@angular/core';
 
 enum GameStatus {
   Start = 'start',
@@ -16,83 +11,17 @@ enum GameStatus {
   templateUrl: './audio-call.component.html',
   styleUrls: ['./audio-call.component.scss'],
 })
-export class AudioCallComponent implements OnDestroy {
+export class AudioCallComponent {
   gameStatus: string;
 
   level = 1;
 
-  page = 0;
-
-  totalScores = 0;
-
-  words: IWord[];
-
-  wordsArray: IWordsInCallGame[];
-
-  singleTask: IWordTask;
-
-  private subscription = new Subscription();
-
-  constructor(private gameService: AudioCallService) {
+  constructor() {
     this.gameStatus = GameStatus.Start;
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 
   gameStart(level: number): void {
     this.level = level;
-    this.page = 0;
-    this.getWords(this.level, this.page);
     this.gameStatus = GameStatus.Game;
-  }
-
-  getWords(level, page): void {
-    this.subscription.add(
-      this.gameService.getWords(level, page).subscribe((data: IWord[]) => {
-        this.words = data;
-        this.wordsArray = this.createTasks();
-        this.singleTask = this.createTask();
-        this.markWordAsShown(this.singleTask.correctIndex);
-      }),
-    );
-  }
-
-  createTasks(): IWordsInCallGame[] {
-    return this.gameService.createTasks(this.words);
-  }
-
-  createTask(): IWordTask {
-    return this.gameService.createTask(this.wordsArray);
-  }
-
-  onNewGameBegin() {
-    this.level = 1;
-    this.page = 0;
-    this.totalScores = 0;
-    this.gameStatus = GameStatus.Start;
-  }
-
-  onCreateNewTask(): void {
-    this.singleTask = this.createTask();
-    this.markWordAsShown(this.singleTask.correctIndex);
-    if (this.checkUnshownWords()) {
-      this.page += 1;
-      this.getWords(this.level, this.page);
-    }
-  }
-
-  onEndGame(e) {
-    this.totalScores = e;
-    this.gameStatus = GameStatus.End;
-  }
-
-  markWordAsShown(index: number): void {
-    this.wordsArray[index].isShown = true;
-  }
-
-  checkUnshownWords(): boolean {
-    return this.gameService.countShown(this.wordsArray);
   }
 }
