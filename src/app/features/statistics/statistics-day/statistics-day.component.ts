463ebc4b7a7071533428a35b7af6a46974402cb5
Maryna Migacheva <m.migacheva@gmail.com>
2021-04-08 11:39:41 +0300

    feat: add template if user have not data

--- src/app/features/statistics/statistics-day/statistics-day.component.ts
+++ src/app/features/statistics/statistics-day/statistics-day.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-statistics-day',
   templateUrl: './statistics-day.component.html',
   styleUrls: ['./statistics-day.component.scss'],
 })
-export class StatisticsDayComponent {}
+export class StatisticsDayComponent {
+  data: number[] = [];
+  // TODO объект с данными для отображения, будет заменен на другой тип, пока только для теста
+
+  constructor(private router: Router) {}
+
+  redirectToLink(link: string) {
+    this.router.navigateByUrl(link);
+  }
+}
