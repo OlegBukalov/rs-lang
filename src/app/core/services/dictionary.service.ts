acd2fd2f9f18486b0778dca87b1c971e8cb58ec2
VanessaGrapefruit <16lexa1602@gmail.com>
2021-04-15 16:51:41 +0300

    refactor: after change request

--- src/app/core/services/dictionary.service.ts
+++ src/app/core/services/dictionary.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/features/auth/auth.service';
 import { environment } from 'src/environments/environment';
 import { DictionaryCategory } from 'src/app/features/dictionary/dictionary-category';
-import { DICTIONARY_FILTERS, getCategoryBody } from 'src/app/features/dictionary/dictonary-filters';
+import { namesByCategory } from 'src/app/features/dictionary/name-by-category';
 import { IWordPage } from '../interfaces/iword-page';
 import { IUserWord } from '../interfaces/iuser-word';
 import { ToasterService } from './toaster.service';
@@ -15,8 +15,6 @@ const MAX_WORDS_PER_PAGE = 3600;
   providedIn: 'root',
 })
 export class DictionaryService {
-  private filters = DICTIONARY_FILTERS;
-
   private get baseUrl() {
     return `${environment.baseUrl}/users/${this.authService.userId}`;
   }
@@ -28,22 +26,9 @@ export class DictionaryService {
   ) {}
 
   getAggregatedWords(category: DictionaryCategory): Observable<IWordPage[]> {
-    const filter = this.getCategoryFilter(category);
+    const filter = `{"userWord.optional.category":"${namesByCategory[category]}"}`;
     const url = `${this.baseUrl}/aggregatedWords/?wordsPerPage=${MAX_WORDS_PER_PAGE}&filter=${filter}`;
     return this.http.get<IWordPage[]>(url);
-  }
-
-  private getCategoryFilter(category: DictionaryCategory): string {
-    switch (category) {
-      case DictionaryCategory.Studied:
-        return this.filters.studied;
-      case DictionaryCategory.Hard:
-        return this.filters.hard;
-      case DictionaryCategory.Deleted:
-        return this.filters.deleted;
-      default:
-        return '';
-    }
   }
 
   async addWordsToDictionary(wordsIdentifiers: string[], category: DictionaryCategory) {
@@ -53,7 +38,7 @@ export class DictionaryService {
   }
 
   async addWordToDictionary(wordId: string, category: DictionaryCategory) {
-    this.addWordAndHandleErrors(wordId, category, true);
+    await this.addWordAndHandleErrors(wordId, category, true);
   }
 
   private async addWordAndHandleErrors(
@@ -74,7 +59,7 @@ export class DictionaryService {
   }
 
   private async tryToAddWordToDictionary(wordId: string, category: DictionaryCategory) {
-    const body = getCategoryBody(category);
+    const body = { optional: { category: namesByCategory[category] } };
     const url = `${this.baseUrl}/words/${wordId}`;
     const isAdded = await this.isAdded(wordId);
     if (isAdded) {
