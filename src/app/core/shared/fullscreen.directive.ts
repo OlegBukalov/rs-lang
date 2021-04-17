c0e16365a87aa7566f6d2475467a86fd57d63f17
Yury Kuzmich <yurakuzmich@gmail.com>
2021-04-18 02:40:53 +0300

    feat: add fullscreen directive

--- src/app/core/shared/fullscreen.directive.ts
+++ src/app/core/shared/fullscreen.directive.ts
@@ -1,13 +1,21 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { ToasterService } from '../services/toaster.service';
 
 @Directive({
   selector: '[appFullscreen]',
 })
 export class FullscreenDirective {
-  constructor(private element: ElementRef, private renderer: Renderer2) {}
+  constructor(private element: ElementRef, private toaster: ToasterService) {}
 
-  @HostListener('click', ['$event']) onClick(event) {
-    const elementToFullscreen = this.renderer.parentNode(event.target);
-    elementToFullscreen.requestFullScreen();
+  @HostListener('click') onClick() {
+    const elementToFullscreen = this.element.nativeElement.parentNode;
+
+    if (!document.fullscreenElement) {
+      elementToFullscreen.requestFullscreen().catch((err) => {
+        this.toaster.showError(`Что-то пошло не так.`, `Ошибка! ${err}`);
+      });
+    } else {
+      document.exitFullscreen();
+    }
   }
 }
