b90ef94d2166b3026aed0f5c5e57d158cfdd2a62
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2021-04-18 20:46:00 +0300

    refactor: copy values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true,
+  baseUrl: 'https://rs-lang-team-5.herokuapp.com',
+  dataURL: 'https://raw.githubusercontent.com/Oubowen/rslang-data/master/',
 };
