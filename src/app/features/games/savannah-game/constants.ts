35ecf74116d2cd27e550f5c2cf090452f52217c9
kirillbuzov73 <kirillbuzov73@gmail.com>
2021-04-18 12:30:59 +0300

    fix(savannah): Fixed game styles

--- src/app/features/games/savannah-game/constants.ts
+++ src/app/features/games/savannah-game/constants.ts
@@ -1,10 +1,8 @@
 import { GameID } from '../../statistics/enums/game-id.enum';
 
-export const MIN_SCREEN_WIDTH = 1150;
-export const MAX_SCREEN_WIDTH = 1500;
-export const MIN_COORDINATE_Y = 230;
-export const MAX_COORDINATE_Y = 305;
-export const STABILIZATION_FACTOR = 3;
+export const MIN_SCREEN_WIDTH = 1536;
+export const MIN_COORDINATE_Y = 310;
+export const MAX_COORDINATE_Y = 530;
 
 export const STEP_Y = 1;
 export const STEP_X = 6;
@@ -14,7 +12,7 @@ export const START_COORDINATE_X = -150;
 export const START_COORDINATE_Y = 50;
 export const ONE_SCORE_POINT = 10;
 export const MAX_WORDS_NUMBER = 4;
-export const MAX_HEALTH = 5;
+export const MAX_HEALTH = 20;
 export const HEALTH_ICON_WIDTH = 40;
 export const DEFAULT_LEVEL = 0;
 export const LEVELS = [0, 1, 2, 3, 4, 5];
